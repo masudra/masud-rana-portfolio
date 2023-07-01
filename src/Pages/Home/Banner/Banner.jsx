b2356d81f667a6a20615468d81d33ecb2aadbcde
masudra <masudrana6278312@gmail.com>
2023-07-01 15:20:32 +0600

    banner add

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,11 +1,39 @@
+import { TypeAnimation } from 'react-type-animation';
 
 const Banner = () => {
-    return (
-        <div>
-            <h1>this is banner</h1>
-            
-        </div>
-    );
+
+  return (
+    <div className='flex  justify-between  px-10 py-2  '>
+      <div className='text-purple-700 font-bold my-auto'>
+        <h1 className='text-4xl text-purple-700'>Hello,<br/> I am  Masud Rana</h1>
+        <TypeAnimation
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            'I am a',
+            1000, // wait 1s before replacing "Mice" with "Hamsters"
+            'Mern Stack Developer',
+            1000,
+            'I am a Front End Developer',
+            1000,
+            'I am a Web Designer',
+            1000
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ fontSize: '2em', display: 'inline-block' }}
+          repeat={Infinity}
+        /><br/>
+
+        <button className='btn bg-purple-700 text-white hover:bg-white hover:text-purple-700 mt-10'>Download Resume</button>
+      </div>
+      <div className='ml-80'>
+        <img className='w-[400px]' src="../../../../public/vs-designer-GIF-unscreen.gif" alt="" />
+      </div>
+
+    </div>
+
+
+  );
 };
 
 export default Banner;
--- src/Pages/Home/ContactUsEmail/ContactUsEmail.jsx
+++ src/Pages/Home/ContactUsEmail/ContactUsEmail.jsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
 
 const ContactUsEmail = () => {
     const form = useRef();
@@ -9,19 +10,28 @@ const ContactUsEmail = () => {
 
         emailjs.sendForm('service_yhotwbt', 'template_9owm68b', form.current, '0Nwt1DDvPHY4j1ddc')
             .then((result) => {
-                console.log(result);
+                if(result.text === 'OK'){
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your Email has been Send',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
             }, (error) => {
-                console.log(error.text);
+                alert(error.text);
             });
+           
     };
 
 
     return (
         <div className="text-center">
             <form ref={form} onSubmit={sendEmail}>
-                <input type="text" name="from_name" placeholder="Enter Your Name" className=" my-2 input input-bordered input-primary w-full max-w-xs" /><br />
-                <input type="email"  name="user_email" placeholder="Enter Your Email" className=" my-2 input input-bordered input-primary w-full max-w-xs" /><br />
-                <input type="text" name="message" placeholder=" Description" className=" my-2 input input-bordered input-primary  w-full h-[120px] max-w-xs" /><br />
+                <input type="text" name="from_name" placeholder="Enter Your Name" className=" my-2 input input-bordered input-primary w-full max-w-xs" required /><br />
+                <input type="email"  name="user_email" placeholder="Enter Your Email" className=" my-2 input input-bordered input-primary w-full max-w-xs" required /><br />
+                <input type="text" name="message" placeholder=" Description" className=" my-2 input input-bordered input-primary  w-full h-[120px] max-w-xs" required /><br />
                 <input className=" my-2 input input-bordered bg-primary  w-full text-white max-w-xs" type="submit" value='Send' />
             </form>
 
